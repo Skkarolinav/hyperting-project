8bb80d02c1ce009f52c872b23058f4be442c9132
SKarolinav <Skkarolinav@gmail.com>
2019-07-10 09:19:42 +0200

    Updates

---
+++ precache-manifest.b4311465da7ebeb44b1b8cafd0e05ac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791ea19abcc5911a0acdd2b456ed778d",
+    "url": "/hyperting-project/index.html"
+  },
+  {
+    "revision": "8ff3b6a324519588b575",
+    "url": "/hyperting-project/static/css/2.dd501b90.chunk.css"
+  },
+  {
+    "revision": "60019c0790104662295c",
+    "url": "/hyperting-project/static/css/main.d133d8a3.chunk.css"
+  },
+  {
+    "revision": "8ff3b6a324519588b575",
+    "url": "/hyperting-project/static/js/2.933f44a1.chunk.js"
+  },
+  {
+    "revision": "60019c0790104662295c",
+    "url": "/hyperting-project/static/js/main.6f23f999.chunk.js"
+  },
+  {
+    "revision": "f3aa3c82ca2f9006d21f",
+    "url": "/hyperting-project/static/js/runtime~main.513e8b51.js"
+  }
+]);
